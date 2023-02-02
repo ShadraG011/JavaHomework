67c303eb07c5e5751fbf783872aac23cb624618e
ShadraG011 <vgardash21@gmail.com>
2023-02-02 16:46:23 +1000

    Задача 12

---
+++ Homework_12/JavaVersion/App.java
@@ -0,0 +1,9 @@
+package Java.JavaHomework.Homework_12.JavaVersion;
+
+
+
+class App extends Controler {
+    void run() {
+        new Viev().menu();
+    }
+}
---
+++ Homework_12/JavaVersion/Program.java
@@ -0,0 +1,8 @@
+package Java.JavaHomework.Homework_12.JavaVersion;
+
+class Program {
+    public static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+}
